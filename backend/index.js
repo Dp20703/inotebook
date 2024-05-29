bd0f2766ba61a51a970e8f7a16ffcadaa80d3ce5
Darshan <darshanrp2003@gmail.com>
2024-05-29 13:02:11 +0530

    User Validation added

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,11 @@ const port = 3000
 app.use(express.json())
 
 //Availabel Routes
+app.get('/', (req, res) => {
+  res.send("helllo World")
+})
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
-})
+}) 
