06ef258aac68af586af3d302c7388816ea126114
Darshan <darshanrp2003@gmail.com>
2024-06-01 17:42:11 +0530

    Added useLocation and useContext

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,10 @@
 import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/NoteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update("Darshan", "12c")
-        // eslint-disable-next-line 
-    }, [])
 
     return (
         <div>
-            This is About {a.state.name} and he is in {a.state.class}
+            This is About page
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,37 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Dp",
-        "class": "5b"
+const notesInitial=[
+    {
+      "_id": "6658ae5dd95f1f81f152b96b",
+      "user": "6658a81d7637eecad30941aa",
+      "title": "mytitle",
+      "description": "my description",
+      "tag": "personal",
+      "date": "2024-05-30T16:50:37.409Z",
+      "__v": 0
+    },
+    {
+      "_id": "6658ae5ed95f1f81f152b96d",
+      "user": "6658a81d7637eecad30941aa",
+      "title": "mytitle",
+      "description": "my description",
+      "tag": "personal",
+      "date": "2024-05-30T16:50:38.052Z",
+      "__v": 0
+    },
+    {
+      "_id": "6658b0bb5d6a81f28938affd",
+      "user": "6658a81d7637eecad30941aa",
+      "title": "new note",
+      "description": "my note",
+      "tag": "personal",
+      "date": "2024-05-30T17:00:43.023Z",
+      "__v": 0
     }
-    const [state, setState] = useState(s1)
-    const update = (name, classN) => {
-        setTimeout(() => {
-            setState({
-                "name": name,
-                "class": classN
-            })
-
-        }, 1000)
-    }
-    return (<NoteContext.Provider value={{ state, update }}>
+  ]
+  const [notes, setNotes] = useState(notesInitial)
+    return (<NoteContext.Provider value={{notes,setNotes}}>
         {props.children}
     </NoteContext.Provider>)
 }
